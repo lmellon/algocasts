cd15cb19c354a7dcaea7e4ec4c86a9c2c87edda5
Lauren Mellon <laumellon@gmail.com>
2019-12-03 15:00:37 -0500

    worked on function to check if string is a palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,49 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+// function palindrome(str) {
+//     let reversed = '';
+//     for (let character in str) {
+//         reversed = character + reversed;
+//     }
+//     if (reversed === str) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// function palindrome(str) {
+//     let arr = str.split('');
+//     arr.reverse();
+//     let reversed = arr.join('');
+//     if (reversed === str) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// // MORE COMPACT syntax
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//     return reversed === str;
+// }
+
+// USING AN ARRAY HELPER FUNCTION "EVERY"
+// this is a solution that does extra comparisons
+//
+function palindrome(str) {
+    // turn string into array so we can use the every helper method
+    // .every() takes two parameters.  the first one is a function that takes the element of the function as its first parameter, and the second parameter is the index of the element.
+    return str.split('').every( (char, i) => {
+        // return comparison of current element (char) against its mirror element at the opposite end of the array.
+        // -i increments for each loop
+        // -1 accounts for the overall length and the 0 inded of JS arrays
+        return char === str[str.length - i - 1];
+    });
+}
+
 
 module.exports = palindrome;
